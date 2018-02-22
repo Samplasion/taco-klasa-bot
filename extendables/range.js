1a56d5a26ff07cfb4736aecffacbcf78426870ba
Glitch (ancient-riddle) <none>
2018-02-22 20:48:58 +0000

    .travis.yml

---
+++ extendables/range.js
@@ -0,0 +1,21 @@
+const { Extendable } = require('klasa');
+
+module.exports = class extends Extendable {
+
+    constructor(...args) {
+        super(...args, ['Message'], {
+            name: 'range',
+            enabled: true,
+            klasa: false
+        });
+    }
+
+    extend(max, min = 0) {
+        const returned = []
+        for (var i = min; i <= max; i++) {
+          returned.push(i)
+        }
+        return returned
+    }
+
+};
