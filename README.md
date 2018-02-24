eafe56a64189ed58a937320f01cb675ae39002f4
Glitch (taco-bot) <none>
2018-02-24 14:53:28 +0000

    🐲🎮 Updated with Glitch

--- README.md
+++ README.md
@@ -4,27 +4,8 @@ taco-klasa-bot
 [![](https://travis-ci.org/Samplasion/taco-klasa-bot.svg?branch=glitch)](https://travis-ci.org/Samplasion/taco-klasa-bot#)
 [![](https://discordapp.com/api/guilds/410854239020253195/embed.png)](https://discord.gg/XdPUzbv)
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
 
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
+Made by Samplasion
+------------------
 
-Find out more [about Glitch](https://glitch.com/about).
-
-
-Your Project
-------------
-
-On the front-end,
-- edit `public/client.js`, `public/style.css` and `views/index.html`
-- drag in `assets`, like images or music, to add them to your project
-
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
-
-
-Made by [Fog Creek](https://fogcreek.com/)
--------------------
-
-\ ゜o゜)ノ
+\ ( ゜o゜)ノ
--- commands/Test/test.js
+++ commands/Test/test.js
@@ -1,4 +1,4 @@
-const { Command } = require('klasa');
+const { Command, RichDisplay } = require('klasa');
 
 module.exports = class extends Command {
 
@@ -17,10 +17,31 @@ module.exports = class extends Command {
             usageDelim: undefined,
             extendedHelp: 'No extended help available.'
         });
+      this.images = [
+          'https://i.imgur.com/gh3vYgj.jpg',
+          'https://i.imgur.com/vBV81m4.jpg',
+          'https://i.imgur.com/92hAsqe.jpg'
+      ];
     }
 
     async run(msg, [...params]) {
-        // This is where you place the code you want to run for your command
+        /*return new RichDisplay()
+            .addPage(new this.client.methods.Embed().setDescription('First page'))
+            .addPage(new this.client.methods.Embed().setDescription('Second page'))
+            .addPage(new this.client.methods.Embed().setDescription('Third page'))
+            .run(await msg.sendMessage('Loading...'));*/
+        const display = new RichDisplay(new this.client.methods.Embed()
+            .setColor(0x673AB7)
+            .setAuthor(this.client.user.username, this.client.user.avatarURL())
+            .setTitle('Norway Pictures Slideshow')
+            .setDescription('Scroll between the images using the provided reaction emotes.')
+        );
+
+        for (let i = 0; i < this.images.length; i++) {
+            display.addPage(template => template.setImage(this.images[i]));
+        }
+
+        return display.run(await msg.sendMessage('Loading slideshow...'));
     }
 
     async init() {
